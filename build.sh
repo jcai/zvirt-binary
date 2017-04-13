2367622eff160cc4d1b46f2099ac65d1eda98474
Jun Tsai <jcai@ganshane.com>
2017-04-13 17:59:47 +0800

    upgrade remote-viewer application
    add amd64
    
    new release

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
+cd armhf
 dpkg -b libspice-client-glib libspice-client-glib-2.0-8_0.31-1~jcai_armhf.deb  
 dpkg -b libspice-client-gtk libspice-client-gtk-3.0-4_0.31-1_armhf.deb        
 dpkg -b virt-viewer virt-viewer_4.0-1~jcai_armhf.deb
+cd -
+cd amd64
+dpkg -b libspice-client-glib libspice-client-glib-2.0-8_0.33-master~jcai_amd64.deb  
+dpkg -b libspice-client-gtk libspice-client-gtk-3.0-5_0.33-master~jcai_amd64.deb        
+dpkg -b virt-viewer virt-viewer_4.0-1~jcai_amd64.deb
 
