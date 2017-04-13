6486140694617322887e69ec9548f20cfc03e84b
Jun Tsai <jcai@ganshane.com>
2017-04-13 14:07:16 +0800

    add ovirt package

---
+++ build.sh
@@ -0,0 +1,4 @@
+dpkg -b libspice-client-glib libspice-client-glib-2.0-8_0.31-1~jcai_armhf.deb  
+dpkg -b libspice-client-gtk libspice-client-gtk-3.0-4_0.31-1_armhf.deb        
+dpkg -b virt-viewer virt-viewer_4.0-1~jcai_armhf.deb
+
