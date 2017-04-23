a85cdfe63c338626e5391851a86b64397a09564d
Jun Tsai <jcai@ganshane.com>
2017-04-23 19:29:26 +0800

    add usb acl

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ cd -
 cd amd64
 dpkg -b libspice-client-glib libspice-client-glib-2.0-8_0.33-master~jcai_amd64.deb  
 dpkg -b libspice-client-gtk libspice-client-gtk-3.0-5_0.33-master~jcai_amd64.deb        
+dpkg -b spice-client-glib-usb-acl-helper spice-client-glib-usb-acl-helper_0.33-master~jcai_amd64.deb        
 dpkg -b virt-viewer virt-viewer_4.0-1~jcai_amd64.deb
 
