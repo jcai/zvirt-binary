fed4401caabc21b3fc27b07103e7d8f54a9d4f7b
Jun Tsai <jcai@ganshane.com>
2017-04-28 16:44:19 +0800

    add usb support for armhf

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 cd armhf
 dpkg -b libspice-client-glib libspice-client-glib-2.0-8_0.31-1~jcai_armhf.deb  
-dpkg -b libspice-client-gtk libspice-client-gtk-3.0-4_0.31-1_armhf.deb        
+dpkg -b libspice-client-gtk libspice-client-gtk-3.0-4_0.31-1~jcai_armhf.deb        
+dpkg -b spice-client-glib-usb-acl-helper spice-client-glib-usb-acl-helper_0.31-1~jcai_armhf.deb        
 dpkg -b virt-viewer virt-viewer_4.0-1~jcai_armhf.deb
 cd -
 cd amd64
